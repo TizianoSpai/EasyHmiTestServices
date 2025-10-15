bc7e50e12e1e6c7723b76cfd053f923147bafeab
Walter <Walter@WIN10-DEV>
2025-10-15 08:58:20 +0200

    Wtr_20251014

--- PlcService/ModBusNetSerivce.cs
+++ PlcService/ModBusNetSerivce.cs
@@ -20,150 +20,204 @@ using XNetClient;
 namespace PlcService
 {
 
-    public class ModbusService
+  public class ModbusService
+  {
+    private ModbusIpMaster? _client;
+    private readonly TcpClient clientTcp;
+    //private readonly DataProvider _dataProv;
+    private readonly CancellationTokenSource _cts = new();
+    public int TestReg { get; set; }
+    public int TestLen { get; set; }
+    public string? ValueReaded { get; set; }
+
+    private volatile object _locker = new object();
+    public event EventHandler ValuesRefreshed;
+    public ConnectionStates ConnectionState { get; private set; }
+
+    public ModbusService(/*MainViewModel viewModel*/)
     {
-        private ModbusIpMaster? _client;
-        private readonly TcpClient clientTcp;
-        //private readonly DataProvider _dataProv;
-        private readonly CancellationTokenSource _cts = new();
-        public int TestReg { get; set; }
-        public int TestLen { get; set; }
-        public string? ValueReaded { get; set; }
-
-        private volatile object _locker = new object();
-        public event EventHandler ValuesRefreshed;
-        public ConnectionStates ConnectionState { get; private set; }
-
-        public ModbusService(/*MainViewModel viewModel*/)
-        {
-            clientTcp = new TcpClient();
-            _client = null;
-        }
-        public async Task<bool> ConnectAsync(string ip, int port)
-        {
-            TraceDbg.TraceON = true;
-            port = 502;
-            try
-            {
-                TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t start {ip}:{port}\n");
-                await clientTcp.ConnectAsync(ip, port);
-                TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t connected \n");
-                _client = ModbusIpMaster.CreateIp(clientTcp);
-                TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t _client {_client}\n");
-                ValueReaded = _client.ToString();
-                TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t ValueReaded {ValueReaded}\n");
-            }
-            catch (Exception ex)
-            {
-                TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t Error: {ex.Message}\n");
-            }
-            return true;
-        }
-        public bool Disconnect()
-        {
-            _cts.Cancel();
-            return true;
-        }
-
-        /// <summary>
-        /// Read all bytes of a db for a specified length. Es.: DB1.DBL256 read 256 bytes in db 1
-        /// </summary>
-        /// <param name="address">Es.: REG1.LEN256 read 256 bytes in db 1</param>
-        /// <returns> byte [] </returns> return the readed buffer
-        public bool[] GetVarArea(string address)
-        {
-            var strings = address.Split('.');
-            int reg = Convert.ToInt32(strings[0].Replace("REG", ""));
-            int len = Convert.ToInt32(strings[1].Replace("LEN", ""));
-            bool[] res = null;
-            try
-            {
-                res = GetVarArea((uint)reg, (uint)len);
-                ValueReaded = res[0] ? "true" : "false";
-
-            }
-            catch (Exception ex)
-            {
-                TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read error {ex.Message}\n");
-                ValueReaded = "Errore";
-            }
-            OnValuesRefreshed();
-            return res;
-        }
-        private bool[] GetVarArea(uint reg, uint length)
-        {
-            TraceDbg.TraceON = true;
-            lock (_locker)
-            {
-                TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read reg:M {reg}-{length} \n");
-                bool[] buffer = new bool[length];
-                try
-                {
-                    if (_client != null)
-                        buffer = _client.ReadCoils((ushort)reg, (ushort)length);
-                    else
-                        buffer[0] = false;
-                }
-                catch (Exception ex)
-                {
-                    TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read error {ex.Message}\n");
-                    throw ex;
-                }
-
-                return buffer;
-            }
-        }
-
-        public async Task StartPollingAsync(string ip, int port, CancellationToken token)
-        {
-            while (!token.IsCancellationRequested)
-            {
-                try
-                {
-                    TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"ReadHoldingRegisters 430 \n");
-                    ushort[] gruppoA = _client.ReadHoldingRegisters(430, 50);
-                    TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"ReadHoldingRegisters 330 \n");
-                    ushort[] gruppoB = _client.ReadHoldingRegisters(330, 1);
-                    ValueReaded = "";
-                    foreach (ushort value in gruppoA)
-                    {
-                        ValueReaded += (char)(value & 0xff);
-                        ValueReaded += (char)(value >> 8 & 0xff);
-                    }
-                    TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"fine conversione \n");
-
-                    //_viewModel.DispatcherService.Invoke(() =>
-                    //{
-                    //    _viewModel.GruppoA = gruppoA;
-                    //    _viewModel.GruppoB = gruppoB;
-                    //});
-                    OnValuesRefreshed();
-                }
-                catch
-                {
-                    // Gestione errori, log o retry
-                }
-
-                await Task.Delay(50, token);
-            }
-        }
-
-
-        public async void StartTest(string ip)
-        {
-            await StartPollingAsync(ip, 0, _cts.Token);
-        }
-
-
-        public void StopTest()
-        {
-            _cts.Cancel();
-        }
-
-        private void OnValuesRefreshed()
-        {
-            ValuesRefreshed?.Invoke(this, new EventArgs());
-        }
+      clientTcp = new TcpClient();
+      _client = null;
     }
+    public async Task<bool> ConnectAsync(string ip, int port)
+    {
+      TraceDbg.TraceON = true;
+      port = 502;
+      try
+      {
+        TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t start {ip}:{port}\n");
+        await clientTcp.ConnectAsync(ip, port);
+        TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t connected \n");
+        _client = ModbusIpMaster.CreateIp(clientTcp);
+        TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t _client {_client}\n");
+        ValueReaded = _client.ToString();
+        TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t ValueReaded {ValueReaded}\n");
+      }
+      catch(Exception ex)
+      {
+        TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"\t Error: {ex.Message}\n");
+      }
+      return true;
+    }
+    public bool Disconnect()
+    {
+      _cts.Cancel();
+      return true;
+    }
+
+    /// <summary>
+    /// Read all bytes of a db for a specified length. Es.: DB1.DBL256 read 256 bytes in db 1
+    /// </summary>
+    /// <param name="address">Es.: REG1.LEN256 read 256 bytes in db 1</param>
+    /// <param name="pars">Es.: IdArea+"-"+Lunghezza</param>
+    /// <returns> byte [] </returns> return the readed buffer
+    public bool[] GetCoilsVarArea(string pars)
+    {
+      var strings = pars.Split('-');
+      int reg = Convert.ToInt32(strings[0].Replace("REG", ""));
+      int len = Convert.ToInt32(strings[1].Replace("LEN", ""));
+      bool[] res = null;
+      try
+      {
+        ValueReaded = "";
+        res = GetCoils((uint)reg, (uint)len);
+        for(int i = 0; i<len; i++)
+          ValueReaded = res[i] ? "true" : "false" + " ";
+
+      }
+      catch(Exception ex)
+      {
+        TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read error {ex.Message}\n");
+        ValueReaded = "Errore";
+      }
+      OnValuesRefreshed();
+      return res;
+    }
+
+    private bool[] GetCoils(uint reg, uint length)
+    {
+      TraceDbg.TraceON = true;
+      lock(_locker)
+      {
+        TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read reg:M {reg}-{length} \n");
+        bool[] buffer = new bool[length];
+        try
+        {
+          if(_client != null)
+          {
+            buffer = _client.ReadCoils((ushort)reg, (ushort)length);
+          }
+          else
+            buffer[0] = false;
+        }
+        catch(Exception ex)
+        {
+          TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read error {ex.Message}\n");
+          throw ex;
+        }
+
+        return buffer;
+      }
+    }
+
+    public ushort[] GetHoldingRegsVarArea(string par)
+    {
+      var strings = par.Split('-');
+      int reg = Convert.ToInt32(strings[0].Replace("REG", ""));
+      int len = Convert.ToInt32(strings[1].Replace("LEN", ""));
+      ushort[] res = null;
+      try
+      {
+        ValueReaded = "";
+        res = GetHoldingRegs((uint)reg, (uint)len);
+        for(int i=0; i<len; i++)
+          ValueReaded = ValueReaded + res[i].ToString() + " ";
+      }
+      catch(Exception ex)
+      {
+        TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read error {ex.Message}\n");
+        ValueReaded = "Errore";
+      }
+      OnValuesRefreshed();
+      return res;
+    }
+
+    private ushort[] GetHoldingRegs(uint reg, uint length)
+    {
+      TraceDbg.TraceON = true;
+      lock(_locker)
+      {
+        TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read reg:M {reg}-{length} \n");
+        ushort[] buffer = new ushort[length];
+        try
+        {
+          if(_client != null)
+          {
+            buffer = _client.ReadHoldingRegisters((ushort)reg, (ushort)length);
+          }
+          else
+            buffer[0] = 0;
+        }
+        catch(Exception ex)
+        {
+          TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t Read error {ex.Message}\n");
+          throw ex;
+        }
+
+        return buffer;
+      }
+    }
+
+    public async Task StartPollingAsync(string ip, int port, CancellationToken token)
+    {
+      while(!token.IsCancellationRequested)
+      {
+        try
+        {
+          TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"ReadHoldingRegisters 430 \n");
+          ushort[] gruppoA = _client.ReadHoldingRegisters(430, 50);
+          TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"ReadHoldingRegisters 330 \n");
+          ushort[] gruppoB = _client.ReadHoldingRegisters(330, 1);
+          ValueReaded = "";
+          foreach(ushort value in gruppoA)
+          {
+            ValueReaded += (char)(value & 0xff);
+            ValueReaded += (char)(value >> 8 & 0xff);
+          }
+          TraceDbg.TRACE(DateTime.Now.ToString("HH:mm:ss") + $"fine conversione \n");
+
+          //_viewModel.DispatcherService.Invoke(() =>
+          //{
+          //    _viewModel.GruppoA = gruppoA;
+          //    _viewModel.GruppoB = gruppoB;
+          //});
+          OnValuesRefreshed();
+        }
+        catch
+        {
+          // Gestione errori, log o retry
+        }
+
+        await Task.Delay(50, token);
+      }
+    }
+
+
+    public async void StartTest(string ip)
+    {
+      await StartPollingAsync(ip, 0, _cts.Token);
+    }
+
+
+    public void StopTest()
+    {
+      _cts.Cancel();
+    }
+
+    private void OnValuesRefreshed()
+    {
+      ValuesRefreshed?.Invoke(this, new EventArgs());
+    }
+  }
 }
 
--- TestModBus/ViewModels/MainWindowViewModel.cs
+++ TestModBus/ViewModels/MainWindowViewModel.cs
@@ -1,178 +1,172 @@
-﻿using PlcService;
-using Prism.Commands;
-using Prism.Mvvm;
-using Sharp7;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows.Input;
+using PlcService;
 using Trace;
 
 namespace SimpleHmi.ViewModels
 {
-    class MainWindowViewModel : BindableBase
+  class MainWindowViewModel : BindableBase
+  {
+    public string IpAddress
     {
-        public string IpAddress
-        {
-            get { return _ipAddress; }
-            set { SetProperty(ref _ipAddress, value); }
-        }
-        private string _ipAddress;
-        public string StartReg
-        {
-            get { return _startReg; }
-            set { SetProperty(ref _startReg, value); }
-        }
-        private string _startReg;
-        public string AreaLength
-        {
-            get { return _areaLength; }
-            set { SetProperty(ref _areaLength, value); }
-        }
-        private string _areaLength;
-        public string MReg
-        {
-            get { return _mReg; }
-            set { SetProperty(ref _mReg, value); }
-        }
-        private string _mReg;
+      get { return _ipAddress; }
+      set { SetProperty(ref _ipAddress, value); }
+    }
+    private string _ipAddress;
+    public string StartReg
+    {
+      get { return _startReg; }
+      set { SetProperty(ref _startReg, value); }
+    }
+    private string _startReg;
+    public string AreaLength
+    {
+      get { return _areaLength; }
+      set { SetProperty(ref _areaLength, value); }
+    }
+    private string _areaLength;
+    public string Params //MReg
+    {
+      get { return pars; }
+      set { SetProperty(ref pars, value); }
+    }
+    private string pars;
 
-        public string ValueReaded
-        {
-            get { return _valueReaded; }
-            set { SetProperty(ref _valueReaded, value); }
-        }
-        private string _valueReaded;
+    public string ValueReaded
+    {
+      get { return _valueReaded; }
+      set { SetProperty(ref _valueReaded, value); }
+    }
+    private string _valueReaded;
 
-        public bool HighLimit
-        {
-            get { return _highLimit; }
-            set { SetProperty(ref _highLimit, value); }
-        }
-        private bool _highLimit;
+    public bool HighLimit
+    {
+      get { return _highLimit; }
+      set { SetProperty(ref _highLimit, value); }
+    }
+    private bool _highLimit;
 
-        public bool LowLimit
-        {
-            get { return _lowLimit; }
-            set { SetProperty(ref _lowLimit, value); }
-        }
-        private bool _lowLimit;
+    public bool LowLimit
+    {
+      get { return _lowLimit; }
+      set { SetProperty(ref _lowLimit, value); }
+    }
+    private bool _lowLimit;
 
-        public bool PumpState
-        {
-            get { return _pumpState; }
-            set { SetProperty(ref _pumpState, value); }
-        }
-        private bool _pumpState;
+    public bool PumpState
+    {
+      get { return _pumpState; }
+      set { SetProperty(ref _pumpState, value); }
+    }
+    private bool _pumpState;
 
-        public int TankLevel
-        {
-            get { return _tankLevel; }
-            set { SetProperty(ref _tankLevel, value); }
-        }
-        private int _tankLevel;
+    public int TankLevel
+    {
+      get { return _tankLevel; }
+      set { SetProperty(ref _tankLevel, value); }
+    }
+    private int _tankLevel;
 
-        public ConnectionStates ConnectionState
-        {
-            get { return _connectionState; }
-            set { SetProperty(ref _connectionState, value); }
-        }
-        private ConnectionStates _connectionState;
+    public ConnectionStates ConnectionState
+    {
+      get { return _connectionState; }
+      set { SetProperty(ref _connectionState, value); }
+    }
+    private ConnectionStates _connectionState;
 
-        public TimeSpan ScanTime
-        {
-            get { return _scanTime; }
-            set { SetProperty(ref _scanTime, value); }
-        }
-        private TimeSpan _scanTime;
+    public TimeSpan ScanTime
+    {
+      get { return _scanTime; }
+      set { SetProperty(ref _scanTime, value); }
+    }
+    private TimeSpan _scanTime;
 
-        public ICommand ConnectCommand { get; private set; }
+    public ICommand ConnectCommand { get; private set; }
 
-        public ICommand DisconnectCommand { get; private set; }
+    public ICommand DisconnectCommand { get; private set; }
 
-        public ICommand StartCommand { get; private set; }
+    public ICommand StartCommand { get; private set; }
 
-        public ICommand StopCommand { get; private set; }
+    public ICommand StopCommand { get; private set; }
 
-        public ICommand ReadRegMCommand { get; private set; }
+    public ICommand ReadCoilsCommand { get; private set; }
+    public ICommand ReadHoldingRegCommand { get; private set; }
 
-        //#define USAS7
+    //#define USAS7
 #if USAS7
         PlcService.S7PlcService _plcService;
 #else
-        PlcService.ModbusService _plcService;
+    PlcService.ModbusService _plcService;
 #endif
 
-        public MainWindowViewModel()
-        {
+    public MainWindowViewModel()
+    {
 #if USAS7
             _plcService = new PlcService();
 #else
-            _plcService = new PlcService.ModbusService();
+      _plcService = new PlcService.ModbusService();
 #endif
-            ConnectCommand = new DelegateCommand(Connect);
-            DisconnectCommand = new DelegateCommand(Disconnect);
-            StartCommand = new DelegateCommand(async () => { await Start(); });
-            StopCommand = new DelegateCommand(async () => { await Stop(); });
-            ReadRegMCommand = new DelegateCommand(async () => { await ReadRegM(); });
+      ConnectCommand = new DelegateCommand(Connect);
+      DisconnectCommand = new DelegateCommand(Disconnect);
+      StartCommand = new DelegateCommand(async () => { await Start(); });
+      StopCommand = new DelegateCommand(async () => { await Stop(); });
+      ReadCoilsCommand = new DelegateCommand(async () => { await ReadCoils(); });
+      ReadHoldingRegCommand = new DelegateCommand(async () => { await ReadHoldingReg(); });
 
-            IpAddress = "10.10.10.11"; 
-            StartReg = "430"; 
-            AreaLength = "50";
+      IpAddress = "192.168.1.211"; //"10.10.10.11"; 
+      StartReg = "1"; //"430"; 
+      AreaLength = "8"; //"50";
 
-            OnPlcServiceValuesRefreshed(null, null);
-            _plcService.ValuesRefreshed += OnPlcServiceValuesRefreshed;
-        }
+      OnPlcServiceValuesRefreshed(null, null);
+      _plcService.ValuesRefreshed += OnPlcServiceValuesRefreshed;
+    }
 
-        private void OnPlcServiceValuesRefreshed(object sender, EventArgs e)
-        {
-            ConnectionState = _plcService.ConnectionState;
-            ValueReaded = _plcService.ValueReaded;
-        }
+    private void OnPlcServiceValuesRefreshed(object sender, EventArgs e)
+    {
+      ConnectionState = _plcService.ConnectionState;
+      ValueReaded = _plcService.ValueReaded;
+    }
 
-        private async void Connect()
-        {
-            await _plcService.ConnectAsync(IpAddress, 501);
-        }
+    private async void Connect()
+    {
+      await _plcService.ConnectAsync(IpAddress, 501);
+    }
 
-        private void Disconnect()
-        {
-            _plcService.Disconnect();
-        }
+    private void Disconnect()
+    {
+      _plcService.Disconnect();
+    }
 
-        private async Task Start()
-        {
-            int Reg;
-            int.TryParse(StartReg, out Reg);
-            int Len;
-            int.TryParse(AreaLength, out Len);
-            TraceDbg.TraceON = true;
-            TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t {IpAddress}-{Reg}:{Len} ");
-            _plcService.StartTest(IpAddress);
-
-        }
-
-        private async Task Stop()
-        {
-            TraceDbg.TraceON = true;
-            TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\tRichiesta stop ");
-            _plcService.StopTest();
-        }
-
-        private async Task ReadRegM()
-        {
-            int Reg;
-            int.TryParse(MReg, out Reg);
-            string TAG_TO_READ = $"REG{Reg}.LEN{1}";
-            _plcService.GetVarArea(IpAddress);
-        }
-
-
+    private async Task Start()
+    {
+      int Reg;
+      int.TryParse(StartReg, out Reg);
+      int Len;
+      int.TryParse(AreaLength, out Len);
+      TraceDbg.TraceON = true;
+      TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\t {IpAddress}-{Reg}:{Len} ");
+      _plcService.StartTest(IpAddress);
 
     }
+
+    private async Task Stop()
+    {
+      TraceDbg.TraceON = true;
+      TraceDbg.DBGTraceLog(DateTime.Now.ToString("HH:mm:ss") + $"\tRichiesta stop ");
+      _plcService.StopTest();
+    }
+
+    private async Task ReadCoils()
+    {
+      int Reg;
+      int.TryParse(Params, out Reg);
+      string TAG_TO_READ = $"REG{Reg}.LEN{1}";
+      //_plcService.GetCoilsVarArea(IpAddress);
+      _plcService.GetCoilsVarArea(Params);
+    }
+
+    private async Task ReadHoldingReg()
+    {
+      _plcService.GetHoldingRegsVarArea(Params);
+    }
+  }
 }
